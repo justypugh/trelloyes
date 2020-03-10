a09085ff7449149b8325dace1801e77e0f81222d
Justin Pugh <justypugh@icloud.com>
2020-03-10 12:52:21 -0700

    trelloyes app assignment initial commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './card.css';
+
+function Card(props) {
+    return (
+        <div className='Card'>
+            <button
+                type='button'
+            >
+                Delete
+            </button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    );
+}
+
+export default Card;
